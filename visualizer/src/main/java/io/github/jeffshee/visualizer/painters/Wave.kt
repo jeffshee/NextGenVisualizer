e81779457540f434911de78f9484ec41ca997478
jeffshee <jeffshee8969@gmail.com>
2019-05-11 00:54:22 +0900

    Some changes in architecture

--- visualizer/src/main/java/io/github/jeffshee/visualizer/painters/Wave.kt
+++ visualizer/src/main/java/io/github/jeffshee/visualizer/painters/Wave.kt
@@ -1,33 +1,42 @@
 package io.github.jeffshee.visualizer.painters
 
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import io.github.jeffshee.visualizer.utils.VisualizerHelper
 
-class Wave(private val paint: Paint) : Painter() {
-    var startHz = 0
-    var endHz = 2000
-    var yR = 0.5f
-    var sliceNum = 256
-    private var width: Float = 0f
-    private var height: Float = 0f
+class Wave(
+    var paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE;style = Paint.Style.STROKE;strokeWidth = 2f
+    },
+    var startHz: Int = 0,
+    var endHz: Int = 2000,
+    var sliceNum: Int = 256,
+    var side: String = "a",
+    var xR: Float = 0f,
+    var yR: Float = .5f,
+    var wR: Float = 1f,
+    var ampR: Float = 1f
+) : Painter() {
+
+    private val path = Path()
 
     @ExperimentalUnsignedTypes
     override fun draw(canvas: Canvas, helper: VisualizerHelper) {
-        // TODO artifacts at the edge of the screen
         val fft = helper.getFftMagnitudeRange(startHz, endHz)
         if (isQuiet(fft)) return
-        width = canvas.width.toFloat()
-        height = canvas.height.toFloat()
+
+        val width = canvas.width.toFloat() * wR
+
         val wave = helper.getWave()
-        val slice = wave.size / sliceNum
+        val point = wave.size / (sliceNum + 1)
         val sliceWidth = width / sliceNum
-        val path = Path()
-        path.moveTo(0f, yR * height)
-        for (i in 0 until sliceNum)
-            path.lineTo(sliceWidth * i, yR * height - wave[slice * i].toUByte().toInt() + 128)
-        path.lineTo(width, yR * height)
-        canvas.drawPath(path, paint)
+
+        path.moveTo(0f, (-wave[0].toUByte().toInt() + 128f) * ampR)
+        for (i in 1..sliceNum)
+            path.lineTo(sliceWidth * i, (-wave[point * i].toUByte().toInt() + 128f) * ampR)
+        drawHelper(canvas, side, xR, yR) { canvas.drawPath(path, paint) }
+        path.reset()
     }
 }
