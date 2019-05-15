bb1ed4f34bb09980a86e9e2811dd8610e030ddbd
jeffshee <jeffshee8969@gmail.com>
2019-05-16 06:12:05 +0900

    [WIP] Add FftCircleWave

---
+++ visualizer/src/main/java/io/github/jeffshee/visualizer/painters/fft/FftCircleWave.kt
@@ -0,0 +1,65 @@
+package io.github.jeffshee.visualizer.painters.fft
+
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import io.github.jeffshee.visualizer.painters.Painter
+import io.github.jeffshee.visualizer.utils.VisualizerHelper
+import kotlin.math.PI
+
+class FftCircleWave(
+    var paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        color = Color.WHITE
+    },
+    var startHz: Int = 0,
+    var endHz: Int = 2000,
+    var sliceNum: Int = 128,
+    var interpolator: String = "sp",
+    var side: String = "a",
+    var xR: Float = .5f,
+    var yR: Float = .5f,
+    var baseR: Float = .4f,
+    var ampR: Float = .6f,
+    var rpm: Float = 1f
+) : Painter() {
+
+    private val path = Path()
+    private var points = Array(0) { GravityModel() }
+    private var rot: Float = 0f
+
+    override fun draw(canvas: Canvas, helper: VisualizerHelper) {
+        val fft = helper.getFftMagnitudeRange(startHz, endHz)
+
+        val circleFft = getCircleFft(fft)
+        if (points.size != circleFft.size) points = Array(circleFft.size) { GravityModel(0f) }
+        points.forEachIndexed { index, bar -> bar.update(circleFft[index].toFloat() * ampR) }
+        val psf = interpolateFftWave(points, sliceNum, interpolator)
+
+        val angle = 2 * PI.toFloat() / sliceNum
+
+        rotateHelper(canvas, rot, xR, yR) {
+            drawHelper(canvas, side, xR, yR, {
+                for (i in 0..sliceNum) {
+                    val point = toCartesian(canvas.width / 2f * baseR + psf.value(i.toDouble()).toFloat(), angle * i)
+                    if (i == 0) path.moveTo(point[0], point[1])
+                    else path.lineTo(point[0], point[1])
+                }
+                path.close()
+                canvas.drawPath(path, paint)
+                path.reset()
+            }, {
+
+            }, {
+
+            })
+        }
+
+        if (rpm != 0f) {
+            rot += rpm / 10f
+            rot %= 360f
+        }
+    }
+
+
+}
---
+++ visualizer/src/main/java/io/github/jeffshee/visualizer/painters/fft/FftCircleWaveRgb.kt
@@ -0,0 +1,48 @@
+package io.github.jeffshee.visualizer.painters.fft
+
+import android.graphics.*
+import io.github.jeffshee.visualizer.painters.Painter
+import io.github.jeffshee.visualizer.utils.VisualizerHelper
+
+class FftCircleWaveRgb(
+    flags: Int = Paint.ANTI_ALIAS_FLAG,
+    startHzR: Int = 0,
+    endHzR: Int = 2000,
+    startHzG: Int = 0,
+    endHzG: Int = 2000,
+    startHzB: Int = 0,
+    endHzB: Int = 2000,
+    sliceNum: Int = 64,
+    interpolator: String = "li",
+    side: String = "a",
+    var xR: Float = .5f,
+    var yR: Float = .5f,
+    var color: List<Int> = listOf(Color.RED, Color.GREEN, Color.BLUE)
+
+) : Painter() {
+
+    private val waveR = FftCircleWave(Paint(flags).apply {
+        style = Paint.Style.FILL;color = this@FftCircleWaveRgb.color[0];xfermode = PorterDuffXfermode(
+        PorterDuff.Mode.ADD
+    )
+    }, startHzR, endHzR, sliceNum, interpolator, side, xR, yR)
+
+    private val waveG = FftCircleWave(Paint(flags).apply {
+        style = Paint.Style.FILL;color = this@FftCircleWaveRgb.color[1];xfermode = PorterDuffXfermode(
+        PorterDuff.Mode.ADD
+    )
+    }, startHzG, endHzG, sliceNum, interpolator, side, xR, yR)
+
+    private val waveB = FftCircleWave(Paint(flags).apply {
+        style = Paint.Style.FILL;color = this@FftCircleWaveRgb.color[2];xfermode = PorterDuffXfermode(
+        PorterDuff.Mode.ADD
+    )
+    }, startHzB, endHzB, sliceNum, interpolator, side, xR, yR)
+
+    override fun draw(canvas: Canvas, helper: VisualizerHelper) {
+        waveR.draw(canvas, helper)
+        rotateHelper(canvas, 5f, xR, yR){waveG.draw(canvas, helper)}
+        rotateHelper(canvas, 10f, xR, yR){waveB.draw(canvas, helper)}
+
+    }
+}
