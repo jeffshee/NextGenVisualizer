a32be293d773c9f30f21e086067cb6c6d22f7e94
jeffshee <jeffshee8969@gmail.com>
2019-06-07 18:43:38 +0900

    [Rotate] add offset

--- visualizer/src/main/java/io/github/jeffshee/visualizer/painters/modifier/Rotate.kt
+++ visualizer/src/main/java/io/github/jeffshee/visualizer/painters/modifier/Rotate.kt
@@ -9,22 +9,24 @@ class Rotate : Painter {
     var xR: Float
     var yR: Float
     var rpm: Float
+    var offset: Float
 
-    constructor(painters: List<Painter>, xR: Float = .5f, yR: Float = .5f, rpm: Float = 1f) {
+    constructor(painters: List<Painter>, xR: Float = .5f, yR: Float = .5f, rpm: Float = 1f, offset: Float = 0f) {
         this.painters = painters
         this.xR = xR
         this.yR = yR
         this.rpm = rpm
+        this.offset = offset
     }
 
-    constructor(painter: Painter, xR: Float = .5f, yR: Float = .5f, rpm: Float = 1f) : this(
-        listOf(painter), xR, yR, rpm
+    constructor(painter: Painter, xR: Float = .5f, yR: Float = .5f, rpm: Float = 1f, offset: Float = 0f) : this(
+        listOf(painter), xR, yR, rpm, offset
     )
 
     private var rot: Float = 0f
 
     override fun draw(canvas: Canvas, helper: VisualizerHelper) {
-        rotateHelper(canvas, rot, xR, yR) {
+        rotateHelper(canvas, rot + offset, xR, yR) {
             painters.forEach { painter ->
                 painter.draw(canvas, helper)
             }
